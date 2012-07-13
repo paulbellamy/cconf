c3b50d45cf02d75391359de28656a362dc706d80
Paul Bellamy <paul.a.bellamy@gmail.com>
2012-07-13 21:29:04 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cconf
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/cconf/core_test.clj
@@ -0,0 +1,7 @@
+(ns cconf.core-test
+  (:use clojure.test
+        cconf.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
