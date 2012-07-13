7573f68700df8427181fee8bd73dc05b4972df8b
Paul Bellamy <paul.a.bellamy@gmail.com>
2012-07-13 22:23:23 +0100

    Adding some failing (not finished) specs

--- src/cconf/core.clj
+++ src/cconf/core.clj
@@ -3,19 +3,19 @@
 (defn argv
   "Load argv data into the config"
   ([options] (argv {} options))
-  ([config options]))
+  ([config options] {}))
 
 (defn env
   "Load environment variables into the config"
   ([options] (env {} options))
-  ([config options]))
+  ([config options] {}))
 
 (defn file
   "Load data from a json file into the config"
   ([filename] (file {} filename))
-  ([config filename]))
+  ([config filename] {}))
 
 (defn defaults
   "Load any default values into the config"
   ([options] (defaults {} options))
-  ([config options]))
+  ([config options] {}))
--- test/cconf/core_test.clj
+++ test/cconf/core_test.clj
@@ -2,6 +2,20 @@
   (:use clojure.test
         cconf.core))
 
-(deftest "loading configs"
+(deftest loading-individual-configs
   (testing "argv"
+    (is (= 0 1)))
+  (testing "env"
+    (is (= 0 1)))
+  (testing "file"
+    (is (= 0 1)))
+  (testing "defaults"
+    (is (= 0 1))))
+
+(deftest merging-configs
+  (testing "non-conflicting keys"
+    (is (= 0 1)))
+  (testing "conflicting keys"
+    (is (= 0 1)))
+  (testing "deeply merging hashes"
     (is (= 0 1))))
